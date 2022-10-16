e6f150861598773dbccf02ac3a9eff4333714afb
NormanZ <1275133384@qq.com>
2022-10-17 00:16:42 +0800

    update dataframe

--- utils/simple_tokenizer.py
+++ utils/simple_tokenizer.py
@@ -9,7 +9,7 @@ import regex as re
 
 @lru_cache()
 def default_bpe():
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "bpe_simple_vocab_16e6.txt.gz")
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "pretrained_model/bpe_simple_vocab_16e6.txt.gz")
 
 
 @lru_cache()
